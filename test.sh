afac028982130d774dfb8737c464826d8309f5f5
Alexandre Boeglin <alex@boeglin.org>
2013-05-29 21:29:11 +0200

    Fancier output

--- test.sh
+++ test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 for i in e*py ; do
+    echo -n $i
+
     itime=$((time python $i) 2>&1 | grep real | grep -v 0m0)
-    [ "x$itime" != "x" ] && echo "$i: $itime"
+    if [ "x$itime" != "x" ] ; then
+        echo ": $itime"
+    else
+        echo -en '\r'
+    fi
 done
 
