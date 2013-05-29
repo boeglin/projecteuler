493b08844d3f1f93229f9685c79a055201789112
Alexandre Boeglin <alex@boeglin.org>
2013-05-29 18:31:04 +0200

    Add comment

--- e021.py
+++ e021.py
@@ -5,15 +5,14 @@ import math
 back = {}
 
 def div(n):
-    acc = 0
-
     if n in back:
         return back[n]
 
+    acc = 0
     for x in xrange(1, int(math.sqrt(n)) + 1):
         if not n % x:
             acc += x
-            if acc / x != x:
+            if acc / x != x: # only add root square once
                 acc += n / x
     back[n] = acc
     return acc
@@ -21,6 +20,6 @@ def div(n):
 acc = 0
 for i in xrange(1, 10000):
     ami = div(i)
-    if i == div(ami) and i != ami:
+    if i != ami and i == div(ami):
         acc += i
 print acc
