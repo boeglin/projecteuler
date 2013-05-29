bd6d07d2e217128081fc2b66d8a69b3a0ff4dc80
Alexandre Boeglin <alex@boeglin.org>
2013-05-29 23:03:15 +0200

    Faster int->list conversion

--- e030.py
+++ e030.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python
 
+import math
+
+def tolist(n):
+    pos = int(math.log10(n))
+
+    while pos >= 0:
+        div = 10 ** pos
+        yield (n / div) ** 5
+        n = n % div
+        pos -= 1
+
 acc = 0
 for i in xrange(2, 200000):
-    msum = sum((int(x) ** 5 for x in list(str(i))))
+    msum = sum(tolist(i))
     if msum == i:
         acc += i
 print acc
--- e034.py
+++ e034.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
-import math 
+import math
+
+def tolist(n):
+    pos = int(math.log10(n))
+
+    while pos >= 0:
+        div = 10 ** pos
+        yield math.factorial(n / div)
+        n = n % div
+        pos -= 1
 
 acc = 0
 for i in xrange(3, 50000):
-    msum = sum((math.factorial(int(x)) for x in list(str(i))))
+    msum = sum(tolist(i))
     if msum == i:
         acc += i
 print acc
